0ddc92d678df68a3cf55531f2c666fb1611db1b2
sim-t <s4sitrum@uni-trier.de>
2019-03-01 17:07:42 +0100

    folder structure changed

---
+++ python/main.py
@@ -0,0 +1,31 @@
+from sklearn.metrics.pairwise import pairwise_distances
+import numpy as np
+
+#need to install pytest: python -m pip install pytest
+
+
+
+# 3 points in dataset
+from algorithms import kMedoid
+
+data = np.array([[1,1,1],
+                [2,2,2],
+                [10,10,10]])
+
+# distance matrix
+D = pairwise_distances(data, metric='euclidean')
+
+# split into 2 clusters
+M, C = kMedoid.kMedoids(D, 2)
+
+print('medoids:')
+for point_idx in M:
+    print( data[point_idx] )
+
+print('')
+print('clustering result:')
+for label in C:
+    for point_idx in C[label]:
+       print('label {0}:　{1}'.format(label, data[point_idx]))
+
+######################################
