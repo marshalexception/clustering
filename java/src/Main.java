91287ca64e05ae1bf2b6ef1fc17f93198b659cbc
MarshalException <42607449+MarshalException@users.noreply.github.com>
2019-04-10 12:34:30 +0200

    matrix lenz

--- java/src/Main.java
+++ java/src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
 
     public static void main(String[] args) {
-        String name = "matrix_new.txt";
-        String path = "data/pfister/" + name;
+        String name = "tmp.csv";
+        String path = "data/" + name;
         String regex = ";";
         FileReader f = new FileReader(path, regex);
 
