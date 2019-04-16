8df1ab607395ef3a41e35cbdffdf8ec735c4e859
MarshalException <42607449+MarshalException@users.noreply.github.com>
2019-04-16 16:33:53 +0200

    deleted unnecessary files

--- python/hierarchical.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plt
-from numpy import genfromtxt
-from scipy.cluster.hierarchy import dendrogram, linkage, fcluster
-from sklearn.metrics import silhouette_score
-
-data_matrix = genfromtxt('..\\data\\symmetrized\\avg_symmetrized_matrix_lenz.csv', delimiter=";")
-data_vectors = genfromtxt('..\\data\\lenz\\vectors_lenz.txt', delimiter=";")
-
